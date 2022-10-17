d0d0e309d1457d77a4b768a00067af450bf0a75b
Tomas Torres <axelagustom07@gmail.com>
2022-10-17 20:28:47 -0300

    Agregando archivos

--- src/App.js
+++ src/App.js
@@ -10,26 +10,35 @@ import Cart from './components/Cart/Cart';
 import { CartProvider } from './contexts/CartContext';
 import CacheContext, { CacheProvider } from './contexts/CacheContext';
 import Checkout from './components/Checkout';
+import FavoritosContext, { FavoritosProvider } from './contexts/FavoritosContext';
+import Favoritos from './components/Favoritos';
+import Condionales from './components/Condionales';
 import DarkProductList from './components/DarkProductList';
 import ItemDetailContainer from './components/ItemDetail/ItemDetailContainer';
 import Intercambiabilidad from './components/Intercambiabilidad';
+
+
+
 function App() {
     return (
       <BrowserRouter>
        <CacheProvider>
           
-        
+       
          <Intercambiabilidad />
         <NavBar />
         <DarkProductList />
-        <Routes>
-          <Route path='/' element={<ItemListContainer greeting={'Bienvenidos a mi tienda'} />} />
-          <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Bienvenidos a mi tienda'} />} />
-          <Route path='/item/:id' element={<ItemDetailContainer />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/checkout' element={<Checkout />} />
-        </Routes>
-       
+        <FavoritosProvider>
+          <Routes>
+            <Route path='/' element={<ItemListContainer greeting={'Bienvenidos a mi tienda'} />} />
+            <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Bienvenidos a mi tienda'} />} />
+            <Route path='/item/:id' element={<ItemDetailContainer />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/checkout' element={<Checkout />} />
+            <Route path='/favs' element={<Favoritos />} />
+          </Routes>
+        </FavoritosProvider>
+        <Condionales />
         </CacheProvider>
       </BrowserRouter>
     );
